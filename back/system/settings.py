c489b68122dbc468d37030c9436a88e704ea4c44
plotzZzky <daniel.plotzky@gmail.com>
2023-12-16 10:15:34 -0300

    Update settings

--- back/system/settings.py
+++ back/system/settings.py
@@ -139,15 +139,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = True
-
-CORS_ALLOW_CREDENTIALS = True
-
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5173',
-    'http://localhost:5173'
-]
-
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:5173',
     'http://127.0.0.1:5173'
